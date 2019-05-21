03402f3e5afb16ba76476966011a65e6ca836808
Fishezzz <33294032+Fishezzz@users.noreply.github.com>
2019-05-21 12:05:57 +0200

    Added icon

--- Project DMX 2.0/Properties/Resources.Designer.cs
+++ Project DMX 2.0/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace Project_DMX_2._0.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon DMX {
+            get {
+                object obj = ResourceManager.GetObject("DMX", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap gear_32xLG {
