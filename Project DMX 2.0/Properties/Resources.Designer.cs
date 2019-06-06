bcdb1131569905608da8ed915ec43f68815f44c7
Fishezzz <33294032+Fishezzz@users.noreply.github.com>
2019-06-06 17:41:37 +0200

    Some adjustments to the GUI

--- Project DMX 2.0/Properties/Resources.Designer.cs
+++ Project DMX 2.0/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace Project_DMX_2._0.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap background {
+            get {
+                object obj = ResourceManager.GetObject("background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon DMX {
